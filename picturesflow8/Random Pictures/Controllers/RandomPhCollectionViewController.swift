913d692c93d3b3b26dd4d8b925e0da95d01f7658
Egor Gorskikh <egorgorskikh@gmail.com>
2021-06-01 11:19:10 +0300

    feat: removed unnecessary

--- picturesflow8/Random Pictures/Controllers/RandomPhCollectionViewController.swift
+++ picturesflow8/Random Pictures/Controllers/RandomPhCollectionViewController.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
-
 class RandomPhCollectionViewController: UICollectionViewController {
     
     private let networkDataFetcher = NetworkDataFetcher()
@@ -109,12 +107,9 @@ class RandomPhCollectionViewController: UICollectionViewController {
         ]
         
     }
-    
-
-    
+    // MARK: - Data Source
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
         return pictures.count
     }
     
@@ -122,7 +117,6 @@ class RandomPhCollectionViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RandomPicuresCell.reuseId, for: indexPath) as! RandomPicuresCell
         let unpashPhoto = pictures[indexPath.item]
         cell.unsplashPhoto = unpashPhoto
-        
         return cell
     }
     
@@ -141,39 +135,6 @@ class RandomPhCollectionViewController: UICollectionViewController {
             selectedPictures.remove(at: index)
         }
     }
-    
-    
-    // MARK: - UICollectionViewDelegate
-    
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
-    
     
 }
 
